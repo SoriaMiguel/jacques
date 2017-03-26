f1fd73842ebe2d3e5ad1320860eb773b704dd2fd
SoriaMiguel <soria.miguel.8@gmail.com>
2017-03-26 16:21:03 -0400

    added id to note

--- app/serializers/note_serializer.rb
+++ app/serializers/note_serializer.rb
@@ -1,5 +1,5 @@
 class NoteSerializer < ActiveModel::Serializer
-  attributes :title, :body, :created_at, :updated_at
+  attributes :title, :body, :created_at, :updated_at, :id
   has_many :tags #says there are a bunch of these.  Make it an array.
-  belongs_to :user # has_one :user There's only one of these 
+  belongs_to :user # has_one :user There's only one of these
 end
